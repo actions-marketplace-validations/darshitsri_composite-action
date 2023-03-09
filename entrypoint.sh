6d96c91be9845a0d34865d9b9ad09ff37fd3a991
Darshit Srivastava <Darshit.Srivastava@lntinfotech.com>
2023-03-09 12:50:49 +0530

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pwd
 ls -lrths
-docker run --platform linux/arm64/v8 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp arm64v8/gcc bash -c "apt update && apt install -y cmake; mkdir build && cmake -B build && cmake --build build --config ${{env.BUILD_TYPE}} && cmake --install build; ./dist/BarcodeReader ./images/AllSupportedBarcodeTypes.png ./license-key.txt"
+docker run --platform linux/arm64/v8 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp arm64v8/gcc 
+bash -c "apt update && apt install -y cmake; mkdir build && cmake -B build && cmake --build build --config "$BUILD_TYPE" && cmake --install build; ./"$DIST" ./"$IMG" ./"$LIC"
