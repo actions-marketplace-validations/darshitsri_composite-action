f95facaf15f0496690ca6bcaaf5c2f78a53aacfa
Darshit Srivastava <Darshit.Srivastava@lntinfotech.com>
2023-03-09 13:37:06 +0530

    My first action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 pwd
 ls -lrths
 docker run --platform linux/arm64/v8 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp arm64v8/gcc 
-bash -c "apt update && apt install -y cmake; mkdir build && cmake -B build && cmake --build build --config "$BUILD_TYPE" && cmake --install build; ./"$DIST" ./"$IMG" ./"$LIC"
+bash -c apt update && apt install -y cmake;
+mkdir build 
+ls -lrths
+cmake -B build
+cmake --build build --config "$BUILD_TYPE" && cmake --install build; ./"$DIST" ./"$IMG" ./"$LIC"
