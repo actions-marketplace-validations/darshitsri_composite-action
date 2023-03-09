1029b45adee330845453aec32f71f9bd4d6f408c
Darshit Srivastava <Darshit.Srivastava@lntinfotech.com>
2023-03-09 13:57:18 +0530

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 pwd
 ls -lrths
 docker run --platform linux/arm64/v8 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp arm64v8/gcc 
-bash -c apt update && apt install -y cmake
+apt update && apt install -y cmake
